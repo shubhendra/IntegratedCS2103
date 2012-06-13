b395280902887ae79950386c1b46b989cb029237
Shubhendra Agrawal <a0065494@nus.edu.sg>
2012-06-13 15:55:08 +0800

    debugged archive

---
+++ CS2103/src/Operation/AgendaEmail.java
@@ -0,0 +1,51 @@
+package operation;
+
+import constant.OperationFeedback;
+import data.Task;
+
+public class AgendaEmail extends Operation implements Runnable{
+
+	@Override
+	public Task[] execute(String userCommand) {
+		// TODO Auto-generated method stub
+		
+		return null;
+	}
+
+	@Override
+	public Task[] undo() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Task[] redo() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean isUndoAble() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public OperationFeedback getOpFeedback() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getOperationName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
--- CS2103/src/Operation/Archive.java
+++ CS2103/src/Operation/Archive.java
@@ -96,12 +96,12 @@ public class Archive extends Operation{
 			logger.debug("running archvie");
 			return archiveTasks();
 		}
-		else if(userCommand.toLowerCase().trim().contains("cleararchive")){
+		else if(userCommand.toLowerCase().trim().contains("clear.archive")){
 			logger.debug("running clear archvie");
 			archiveCommand=archiveStatus.CLEAR_ARCHIVE;
 			return clearArchive();
 		}
-		else if(userCommand.toLowerCase().trim().contains("importarchive")){
+		else if(userCommand.toLowerCase().trim().contains("import.archive")){
 			archiveCommand=archiveStatus.IMPORT_ARCHIVE;
 			logger.debug("importing archive");
 			return importArchive();
@@ -141,6 +141,7 @@ public class Archive extends Operation{
 		// TODO Auto-generated method stub
 		isUndoAble=false;
 		StorageManager.clearArchive();
+		feedback=OperationFeedback.VALID;
 		return new Task[1];
 		
 	}
